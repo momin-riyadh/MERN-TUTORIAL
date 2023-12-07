9e31ecc1fd3d5b3f9c8f73c380c1ae4003abf943
Momin Riyadh <mominriyadh@gmail.com>
2023-12-07 16:44:45 +0600

    Add error handling middleware documentation
    A detailed explanation was added to the error middleware function.

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,3 +1,11 @@
+/**
+ * Handles error and sends response with appropriate status code and error message.
+ * @param {*} err - The error object.
+ * @param {*} req - The request object.
+ * @param {*} res - The response object.
+ * @param {*} next - The next middleware function.
+ * @returns {void}
+ */
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
     res.status(statusCode)
