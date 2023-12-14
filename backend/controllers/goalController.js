ed7afa1fe672f9558e7ecaaa290b33e0bea4c95e
momin-riyadh <mominriyadh@gmail.com>
2023-12-14 12:02:51 +0600

    JWT Protected Routes
    Successfully Created  Protected User Routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,7 +9,7 @@ const Goal = require('../models/goalModel');
  * @returns {void}
  */
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
     res.status(200).json({goals})
 });
 /**
@@ -26,7 +26,8 @@ const setGoal = asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json({goal})
 })
