358b4c2a17364370a3de8a517617c18a23902f32
momin-riyadh <mominriyadh@gmail.com>
2023-12-17 20:33:01 +0600

    Frontend
    Login and Register components has been update

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -5,7 +5,7 @@ import authService from './authService'
 const user = JSON.parse(localStorage.getItem('user'));
 
 const initialState = {
-    user:  user || null,
+    user: user || null,
     isError: false,
     isSuccess: false,
     isLoading: false,
@@ -17,6 +17,17 @@ const initialState = {
 export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
     try {
         return await authService.register(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+//Login User
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+    try {
+        return await authService.login(user)
     } catch (error) {
         const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
 
@@ -51,6 +62,20 @@ export const authSlice = createSlice({
                 state.user = action.payload
             })
             .addCase(register.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+            .addCase(login.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
