ecd9613b256f02e731892400789d12c879c1de74
Momin Riyadh <mominriyadh@gmail.com>
2023-12-26 14:57:11 +0600

    fix and deploy package ui

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,11 +1,11 @@
 import {useDispatch} from "react-redux";
 import {deleteGoal} from "../features/goals/goalSlice"
 
-function GoalItem({goal}){
+function GoalItem({ goal }){
     const dispatch = useDispatch()
     return (
         <div className="goal">
-            <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
+            <small>{new Date(goal.createdAt).toLocaleString('en-US')}</small>
             <h2>{goal.text}</h2>
             <button onClick={()=>dispatch(deleteGoal(goal._id))} className="close">X</button>
         </div>
